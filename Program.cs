7ee47899957eb8bfd433a1e012f5756e52ca3d28
EngAmmarAtamesh <am8952@fayoum.edu.eg>
2026-09-23 15:29:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SmartDeliveryManagementSystem
+{
+    // ============================================
+    // DeliveryAddress Struct
+    // ============================================
+    public struct DeliveryAddress
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+
+        public DeliveryAddress(string street, string city)
+        {
+            if (string.IsNullOrWhiteSpace(street))
+                throw new ArgumentException("Street cannot be empty.");
+
+            if (string.IsNullOrWhiteSpace(city))
+                throw new ArgumentException("City cannot be empty.");
+
+            Street = street;
+            City = city;
+        }
+
+        public override string ToString()
+        {
+            return $"{Street}, {City}";
+        }
+    }
